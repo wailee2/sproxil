3a7b12ed1235fd09d346adc71a141d32a4cdea9e
Feranmi <9fernni@gmail.com>
2025-10-16 17:31:48 +0100

    fixed background image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
                     </div>
                     <p className="text-fluid-body text-balance  max-w-lg">Secure product verification, protect supply chains, and turn each mobile scan into a trusted connection between your brand and customers.</p>
                 </div>
-                <div className="mt-16 lg:mt-18 w-full bg-[url('/herobg.jpg')] bg-cover bg-center h-[90vh] lg:h-[70vh] rounded-bl-2xl lg:rounded-bl-4xl rounded-br-[5rem] lg:rounded-br-[10rem] rounded-tl-[5rem] lg:rounded-tl-[10rem] rounded-tr-2xl lg:rounded-tr-4xl ">
+                <div className="mt-16 lg:mt-18 w-full bg-[url('/herobg.jpg')] bg-cover bg-center h-[65vh] lg:h-[70vh] rounded-bl-2xl lg:rounded-bl-4xl rounded-br-[5rem] lg:rounded-br-[10rem] rounded-tl-[5rem] lg:rounded-tl-[10rem] rounded-tr-2xl lg:rounded-tr-4xl ">
                 </div>
             </div>
         </section>
